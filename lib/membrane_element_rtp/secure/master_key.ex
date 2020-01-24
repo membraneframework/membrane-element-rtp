590d12a88376f3c2b507b19ca85b6bc4e0ff3abe
Bartosz Sofiński <bartosz.sofinski@swmansion.com>
2020-01-24 15:22:00 +0100

    Remove session keys from MasterKey struct

--- lib/membrane_element_rtp/secure/master_key.ex
+++ lib/membrane_element_rtp/secure/master_key.ex
@@ -1,20 +1,17 @@
 defmodule Membrane.Element.RTP.Parser.Secure.MasterKey do
   @moduledoc """
+  A struct for a master key-salt pair.
   """
-
-  @type session_keys_t() :: %{keys: map(), lifetime: integer()}
 
   @type t :: %__MODULE__{
           key: binary(),
           salt: binary(),
           key_derivation_rate: non_neg_integer(),
-          packet_count: non_neg_integer(),
-          session_keys: session_keys_t()
+          packet_count: non_neg_integer()
         }
 
   defstruct key: nil,
             salt: nil,
             key_derivation_rate: 0,
-            packet_count: 0,
-            session_keys: nil
+            packet_count: 0
 end
